25c5e844dde24cf0a33ff4e55b021fea34410ccb
18023217296 <841223400@qq.com>
2022-08-22 17:09:28 +0800

    logger module

---
+++ logger.py
@@ -0,0 +1,12 @@
+import logging
+
+
+def getDefaultLog():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level = logging.INFO)
+    handler = logging.FileHandler("log.txt")
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
